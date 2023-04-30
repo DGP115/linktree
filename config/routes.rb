d750a20cd0d122521943f4ac070b128f1ffe88cd
DGP115 <david.g.peloso@gmail.com>
2023-04-30 14:01:45 -0400

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'dashboard#index'
+
+  devise_for :users
+end
